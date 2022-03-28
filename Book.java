a9cb01d2528bcdec0cddb73580bbf77162648976
Pedro Fernandes <61127870+pedro-fernandes-11@users.noreply.github.com>
2022-03-28 19:27:33 -0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,41 @@
+package javaapplication;
+
+public class Book {
+    private String title;
+    private int year;
+    private boolean isImported;
+    
+    public Book(String title, int year, boolean isImported){
+        this.title = title;
+        this.year = year;
+        this.isImported = isImported;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isIsImported() {
+        return isImported;
+    }
+
+    public void setIsImported(boolean isImported) {
+        this.isImported = isImported;
+    }
+    
+    public String toString(){
+        return this.title+" "+this.year+" "+this.isImported;
+    }
+}
